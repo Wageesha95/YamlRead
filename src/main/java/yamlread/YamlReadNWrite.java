6574184f0728f57b6cf461327b26ae3bb84090ff
Wageesha95 <imwageesha@gmail.com>
2019-09-03 14:12:36 +0530

    print 2 files without separating nested list

--- src/main/java/yamlread/YamlReadNWrite.java
+++ src/main/java/yamlread/YamlReadNWrite.java
@@ -3,6 +3,7 @@ package yamlread;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.yaml.snakeyaml.Yaml;
 
@@ -19,11 +20,34 @@ public class YamlReadNWrite {
 	  }
 	*/
 	
+	
 	public static void main(String[] args) {
 	Yaml yaml = new Yaml();
 	InputStream inputStream = yaml.getClass().getClassLoader().getResourceAsStream("person.yaml");
-	Map<String, Object> obj = (Map<String, Object>) yaml.load(inputStream);
-	System.out.println(obj);
-
+	Map<String, Object> obj_1 = (Map<String, Object>) yaml.load(inputStream);
+	//System.out.println(obj_1);
+	
+	InputStream inputStream_2 = yaml.getClass().getClassLoader().getResourceAsStream("person2.yaml");
+	Map<String, Map<String, Object>> obj_2 = (Map<String, Map<String, Object>>) yaml.load(inputStream_2);
+	
+	for(Map.Entry m:obj_1.entrySet()) {
+	System.out.println(m.getKey() + "   " + m.getValue());	
 	}
+	
+	for(Map.Entry m:obj_2.entrySet() ) {
+	System.out.println(m.getKey() + "   " + m.getValue());	
+		}
+	
+	}
+		
 }
+
+
+	
+	
+	 
+	
+	
+	
+	
+	
